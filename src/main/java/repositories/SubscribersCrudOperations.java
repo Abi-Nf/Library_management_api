ab5ecdfacdd774f657bd58add44a3c1bde12a452
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2023-11-17 20:55:00 +0300

    feat: add CRUD operations for subscribers

---
+++ src/main/java/repositories/SubscribersCrudOperations.java
@@ -0,0 +1,27 @@
+package repositories;
+
+import models.Subscribers;
+
+import java.util.List;
+
+public class SubscribersCrudOperations implements CrudOperations<Subscribers> {
+    @Override
+    public Subscribers save(Subscribers value) {
+        return null;
+    }
+
+    @Override
+    public List<Subscribers> saveAll(List<Subscribers> values) {
+        return null;
+    }
+
+    @Override
+    public List<Subscribers> findAll() {
+        return null;
+    }
+
+    @Override
+    public Subscribers delete(Subscribers value) {
+        return null;
+    }
+}
