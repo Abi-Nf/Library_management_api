576f01bc0be7ad956cdacffa27d50be3e41be627
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2023-11-17 19:30:10 +0300

    feat: add DatabaseConnection class to access postgresql server by Driver and environnement variables values

---
+++ src/main/java/configurations/DatabaseConnection.java
@@ -0,0 +1,20 @@
+package configurations;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    public static Connection connect(){
+        String url = System.getenv("PSQL_URL");
+        String user = System.getenv("PSQL_USER");
+        String password = System.getenv("PSQL_PASS");
+        try {
+            return DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+            return null;
+        }
+    }
+}
