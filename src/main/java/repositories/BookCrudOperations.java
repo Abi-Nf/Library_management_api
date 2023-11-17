b6fcf78ba566fcc467dcf85c9353c6d90430ca8b
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2023-11-17 20:53:52 +0300

    feat: add CRUD operations for books

---
+++ src/main/java/repositories/BookCrudOperations.java
@@ -0,0 +1,27 @@
+package repositories;
+
+import models.Book;
+
+import java.util.List;
+
+public class BookCrudOperations implements CrudOperations<Book> {
+    @Override
+    public Book save(Book value) {
+        return null;
+    }
+
+    @Override
+    public List<Book> saveAll(List<Book> values) {
+        return null;
+    }
+
+    @Override
+    public List<Book> findAll() {
+        return null;
+    }
+
+    @Override
+    public Book delete(Book value) {
+        return null;
+    }
+}
