0c2018aa16ed093f6128bd6843c423883457e178
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2023-11-18 12:34:21 +0300

    feat: add parseSubscriber private method

--- src/main/java/repositories/SubscribersCrudOperations.java
+++ src/main/java/repositories/SubscribersCrudOperations.java
@@ -2,9 +2,21 @@ package repositories;
 
 import models.Subscribers;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class SubscribersCrudOperations implements CrudOperations<Subscribers> {
+
+    private Subscribers parseSubscriber(ResultSet results) throws SQLException {
+        Subscribers subscribers = new Subscribers();
+        subscribers.setId(results.getLong("id"));
+        subscribers.setName(results.getString("name"));
+        subscribers.setReference(results.getString("reference"));
+        return subscribers;
+    }
+
+
     @Override
     public Subscribers save(Subscribers value) {
         return null;
