ee39dcc32365dbe5c1a7c364c01390acfeec43b0
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2023-11-18 11:25:53 +0300

    feat: implement generic interface CrudOperations with type Book and Override all to make fully featured

--- src/main/java/repositories/BookCrudOperations.java
+++ src/main/java/repositories/BookCrudOperations.java
@@ -5,8 +5,10 @@ import models.Author;
 import models.Book;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
-public class BookCrudOperations {
+public class BookCrudOperations implements CrudOperations<Book> {
     private final Connection connection = DbConnect.connect();
 
     private ResultSet statementFinding(String sql, Book book) throws SQLException {
@@ -61,5 +63,81 @@ public class BookCrudOperations {
         return newBook;
     }
 
-    // redundancies
+
+    @Override
+    public Book save(Book value) {
+        String sql = String.format(
+                """
+                insert into "book" (bookname, pagenumbers, topic, releasedate)
+                values (?, ?, '%s'::topic, ?);
+                """,
+                value.getTopic()
+        );
+        try {
+            assert this.connection != null;
+            PreparedStatement statement = this.connection.prepareStatement(sql);
+            statement.setString(1, value.getBookName());
+            statement.setInt(2, value.getPageNumbers());
+            statement.setDate(3, Date.valueOf(value.getReleaseDate()));
+
+            boolean isInserted = statement.executeUpdate() == 1;
+            if(isInserted) return this.find(value);
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    @Override
+    public List<Book> saveAll(List<Book> values) {
+        List<Book> books = new ArrayList<>();
+        for (Book book : values) {
+            Book saved = this.save(book);
+            if(saved != null) books.add(saved);
+        }
+        return books;
+    }
+
+    @Override
+    public List<Book> findAll() {
+        List<Book> bookList = new ArrayList<>();
+        String sql = """
+                select book.id as book_id, a.id as author_id, * from "book"
+                full outer join author a on a.id = book.author
+                """;
+        try {
+            ResultSet result = this.statementFinding(sql);
+            while(result.next()) bookList.add(
+                    this.parseBook(result)
+            );
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return bookList;
+    }
+
+    @Override
+    public Book delete(Book value) {
+        Book toBeDeleted = this.find(value);
+        String sql = String.format(
+                """
+                delete from "book" where bookname = ? and pagenumbers = ? and topic = '%s'::topic and releasedate = ?
+                """,
+                value.getTopic()
+        );
+
+        try {
+            assert this.connection != null;
+            PreparedStatement statement = this.connection.prepareStatement(sql);
+            statement.setString(1, value.getBookName());
+            statement.setInt(2, value.getPageNumbers());
+            statement.setDate(4, Date.valueOf(value.getReleaseDate()));
+
+            boolean isDeleted = statement.executeUpdate() == 1;
+            if(isDeleted) return toBeDeleted;
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
 }
